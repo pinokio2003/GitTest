71d5a51337e8e767902b9c9873f62ada53dd93a0
pinokio2003 <pinokio0476@gmail.com>
2023-01-30 23:50:14 +0200

    add ViewController background gray color

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+private extension ViewController{
+    private func setupBackground(){
+        view.backgroundColor = .systemGray5
+    }
+}
