8eb99b316c926b9d7360eb3449a1f55dd896d1d5
pinokio2003 <pinokio0476@gmail.com>
2023-01-31 23:13:59 +0200

    for govan

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,14 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tablewView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray5
+        setupTableView()
     }
 }
 
 private extension ViewController{
-    private func setupBackground(){
-        view.backgroundColor = .systemGray5
+    private func setupTableView(){
+        tablewView = UITableView(frame: CGRect(x: 0,
+                                               y: 65,
+                                               width: view.bounds.width,
+                                               height: view.bounds.height) , style: .grouped)
+        view.addSubview(tablewView)
+        tablewView.backgroundColor = .gray
+        tablewView.delegate = self
+        tablewView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tablewView.dataSource = self
+        
     }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+    
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "111"
+        return cell
+    }
+    
+    
 }
